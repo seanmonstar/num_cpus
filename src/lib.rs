e96e7f27b6e755af188077cf8b460c5ca19694df
Peter Atashian <retep998@gmail.com>
2017-12-18 03:15:16 -0500

    Use winapi

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,29 @@
 //! A crate with utilities to determine the number of CPUs available on the
 //! current system.
-//! 
+//!
 //! Sometimes the CPU will exaggerate the number of CPUs it contains, because it can use
-//! [processor tricks] to deliver increased performance when there are more threads. This 
+//! [processor tricks] to deliver increased performance when there are more threads. This
 //! crate provides methods to get both the logical and physical numbers of cores.
-//! 
+//!
 //! This information can be used as a guide to how many tasks can be run in parallel.
 //! There are many properties of the system architecture that will affect parallelism,
 //! for example memory access speeds (for all the caches and RAM) and the physical
 //! architecture of the processor, so the number of CPUs should be used as a rough guide
 //! only.
-//! 
+//!
 //!
 //! ## Examples
 //!
 //! Fetch the number of logical CPUs.
-//! 
+//!
 //! ```
 //! let cpus = num_cpus::get();
 //! ```
-//! 
+//!
 //! See [`rayon::Threadpool`] for an example of where the number of CPUs could be
 //! used when setting up parallel jobs (Where the threadpool example uses a fixed
 //! number 8, it could use the number of CPUs).
-//! 
+//!
 //! [processor tricks]: https://en.wikipedia.org/wiki/Simultaneous_multithreading
 //! [`rayon::ThreadPool`]: https://docs.rs/rayon/0.8.2/rayon/struct.ThreadPool.html
 #![cfg_attr(test, deny(warnings))]
@@ -33,15 +33,17 @@
 
 #[cfg(not(windows))]
 extern crate libc;
+#[cfg(windows)]
+extern crate winapi;
 
 
 /// Returns the number of available CPUs of the current system.
-/// 
+///
 /// This function will get the number of logical cores. Sometimes this is different from the number
 /// of physical cores (See [Simultaneous multithreading on Wikipedia][smt]).
-/// 
+///
 /// # Examples
-/// 
+///
 /// ```
 /// let cpus = num_cpus::get();
 /// if cpus > 1 {
@@ -53,9 +55,9 @@ extern crate libc;
 ///
 /// # Note
 ///
-/// This will check [sched affinity] on Linux, showing a lower number of CPUs if the current 
-/// thread does not have access to all the computer's CPUs. 
-/// 
+/// This will check [sched affinity] on Linux, showing a lower number of CPUs if the current
+/// thread does not have access to all the computer's CPUs.
+///
 /// [smt]: https://en.wikipedia.org/wiki/Simultaneous_multithreading
 /// [sched affinity]: http://www.gnu.org/software/libc/manual/html_node/CPU-Affinity.html
 #[inline]
@@ -71,15 +73,15 @@ pub fn get() -> usize {
 /// On other platforms, or if the physical count fails on supported platforms,
 /// this function returns the same as [`get()`], which is the number of logical
 /// CPUS.
-/// 
+///
 /// # Examples
-/// 
+///
 /// ```
 /// let logical_cpus = num_cpus::get();
 /// let physical_cpus = num_cpus::get_physical();
 /// if logical_cpus > physical_cpus {
 ///     println!("We have simultaneous multithreading with about {:.2} \
-///               logical cores to 1 physical core.", 
+///               logical cores to 1 physical core.",
 ///               (logical_cpus as f64) / (physical_cpus as f64));
 /// } else if logical_cpus == physical_cpus {
 ///     println!("Either we don't have simultaneous multithreading, or our \
@@ -89,7 +91,7 @@ pub fn get() -> usize {
 ///               some of the CPUs on our system.");
 /// }
 /// ```
-/// 
+///
 /// [`get()`]: fn.get.html
 #[inline]
 pub fn get_physical() -> usize {
@@ -119,23 +121,8 @@ fn get_num_physical_cpus_windows() -> Option<usize> {
     use std::ptr;
     use std::mem;
 
-    #[allow(non_upper_case_globals)]
-    const RelationProcessorCore: u32 = 0;
-
-    #[repr(C)]
-    #[allow(non_camel_case_types)]
-    struct SYSTEM_LOGICAL_PROCESSOR_INFORMATION {
-        mask: usize,
-        relationship: u32,
-        _unused: [u64; 2]
-    }
-
-    extern "system" {
-        fn GetLogicalProcessorInformation(
-            info: *mut SYSTEM_LOGICAL_PROCESSOR_INFORMATION,
-            length: &mut u32
-        ) -> u32;
-    }
+    use winapi::um::sysinfoapi::GetLogicalProcessorInformation;
+    use winapi::um::winnt::{RelationProcessorCore, SYSTEM_LOGICAL_PROCESSOR_INFORMATION};
 
     // First we need to determine how much space to reserve.
 
@@ -177,7 +164,7 @@ fn get_num_physical_cpus_windows() -> Option<usize> {
 
     let phys_proc_count = buf.iter()
         // Only interested in processor packages (physical processors.)
-        .filter(|proc_info| proc_info.relationship == RelationProcessorCore)
+        .filter(|proc_info| proc_info.Relationship == RelationProcessorCore)
         .count();
 
     if phys_proc_count == 0 {
